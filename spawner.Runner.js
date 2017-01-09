96d90b05bb6e6dca41c69d7dc0788953f46f8570
Eldalar <kai837@googlemail.com>
2017-01-10 00:57:21 +0100

    Optimized Runner code
    Prevented it from always using one more MOVE than necessary

--- spawner.Runner.js
+++ spawner.Runner.js
@@ -2,7 +2,7 @@ var spawnerBase = require( "spawner.Base" );
 
 module.exports = {
     spawn: function( room ) {
-        var runnerLoadout = spawnerBase.balanceLoadout( [MOVE], { MOVE: 1, CARRY:1 }, room.energyCapacityAvailable );
+        var runnerLoadout = spawnerBase.balanceLoadout( [], { MOVE: 1, CARRY:1 }, room.energyCapacityAvailable );
         var name = Game.spawns['Spawn1'].createCreep( runnerLoadout, undefined, {role: 'runner' } );
         if( name != -6 &&
             name != -4) {
