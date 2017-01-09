74ea4758659117cfb072fb4e963ba0138b9ce152
Eldalar <kai837@googlemail.com>
2017-01-10 00:55:30 +0100

    Optimized the miner spawning code
    Should now produce 4 Move and 4 Work at most, instead of 12 WORK and
    1 MOVE, reducing the cost from 1250 to 600

--- spawner.Miner.js
+++ spawner.Miner.js
@@ -2,8 +2,24 @@ module.exports = {
     createMinerLoadout( room ) {
         var retval = [ MOVE ];
         var energyAvailable = room.energyCapacityAvailable - 50;
-        for( i = 0; i < Math.floor(energyAvailable / 100); i++ ) {
+        var numWorks = Math.floor(energyAvailable / 100);
+        // 3000 Energy regenerate every 500 ticks, so optimum is 6 energy per
+        // tick, 1 work = 2 energy/tick, so 3 at optimum. Taking 4 to be safe
+        // leaving a buffer of 125 ticks for travel etc.
+        if( numWorks > 4 ) {
+          numWorks = 4;
+        }
+        for( i = 0; i < numWorks; i++ ) {
             retval.push( WORK );
+        }
+        energyAvailable -= numWorks * 100;
+        var numMoves = Math.floor(energyAvailable / 50);
+        // One per Work, but we added one in the beginning so 3 instead of 4
+        if( numMoves > 3 ) {
+          numMoves = 3;
+        }
+        for( i = 0; i < numMoves; i++ ) {
+          retval.push( MOVE );
         }
         return retval;
     },
