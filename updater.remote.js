4c5d49f8ca8086a5c89666b6b3b361e8d0f51b3f
Eldalar <kai837@googlemail.com>
2017-01-09 23:50:07 +0100

    Improved update.remote.js

--- updater.remote.js
+++ updater.remote.js
@@ -3,12 +3,18 @@
  **/
 module.exports = {
     run: function(room) {
+      if( room.memory.RegisteredRemoteFlags ) {
+        room.memory.RegisteredRemoteFlags = {};
+      }
       var counter = 1;
       while( true ) {
         var remoteName = "remote." + room.name + "." + counter;
         var remoteFlag = Game.flags[remoteName];
         if( remoteFlag ) {
-          console.log(JSON.stringify( remoteFlag ));
+          var remoteFlagInfo = room.memory.RegisteredRemoteFlags[ remoteName ];
+          if( !remoteFlagInfo ) {
+            console.log(JSON.stringify( remoteFlagInfo ));
+          }
         } else {
           break;
         }
