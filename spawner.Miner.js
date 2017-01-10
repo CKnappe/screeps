0ea571af589738f8c11191573ddbf90fce10a755
Eldalar <kai837@googlemail.com>
2017-01-10 08:18:05 +0100

    Bugfix in spawner.Miner.js
    Mistook the respawn for 500 ticks instead of the real 300 ticks, making
    the miners too small.
    Increased from 4 WORK/MOVE to 6 WORK/MOVE and the cost from 600 to 900

--- spawner.Miner.js
+++ spawner.Miner.js
@@ -3,11 +3,11 @@ module.exports = {
         var retval = [ MOVE ];
         var energyAvailable = room.energyCapacityAvailable - 50;
         var numWorks = Math.floor(energyAvailable / 100);
-        // 3000 Energy regenerate every 500 ticks, so optimum is 6 energy per
-        // tick, 1 work = 2 energy/tick, so 3 at optimum. Taking 4 to be safe
-        // leaving a buffer of 125 ticks for travel etc.
-        if( numWorks > 4 ) {
-          numWorks = 4;
+        // 3000 Energy regenerate every 300 ticks, so optimum is 10 energy per
+        // tick, 1 work = 2 energy/tick, so 5 at optimum. Taking 6 to be safe
+        // leaving a buffer of 50 ticks for travel etc.
+        if( numWorks > 6 ) {
+          numWorks = 6;
         }
         for( i = 0; i < numWorks; i++ ) {
             retval.push( WORK );
@@ -15,8 +15,8 @@ module.exports = {
         energyAvailable -= numWorks * 100;
         var numMoves = Math.floor(energyAvailable / 50);
         // One per Work, but we added one in the beginning so 3 instead of 4
-        if( numMoves > 3 ) {
-          numMoves = 3;
+        if( numMoves > 5 ) {
+          numMoves = 5;
         }
         for( i = 0; i < numMoves; i++ ) {
           retval.push( MOVE );
